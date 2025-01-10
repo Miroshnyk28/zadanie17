4235def174e63d67300c6209e5b6d62cbf0b42ea
Your Name <you@example.com>
2025-01-10 11:30:31 +0100

    Polepszenia kodu

--- plik.c
+++ plik.c
@@ -1,15 +1,24 @@
 #include <stdio.h>
-float volume_cuboid(float x, float y, float h) {
+#include <math.h>
 
+float volume_cuboid(float a, float b, float c) {
+    return a * b * c;
+}
+float volumeKula(float r) {
+    return (4.0 / 3.0) * M_PI * (r*r*r);
 }
 int main() {
-float x,y,h;
-printf("Podaj dlugosc podstawy prostopadloscianu: ");
-scanf("%f", &x);
-printf("Podaj szerokosc podstawy prostopadloscianu: ");
-scanf("%f", &y);
-printf("Podaj wysokosc prostopadloscianu: ");
-scanf("%f", &h);
-printf("Objectosc prostopadloscianu : %f\n", volume_cuboid(x, y, h));
-return 0;
+    float a, b, c, r;
+     printf("Podaj dlugosc prostopadloscianu: ");
+        scanf("%f", &a);
+     printf("Podaj szerokosc prostopadloscianu: ");
+        scanf("%f", &b);
+     printf("Podaj wysokosc prostopadloscianu: ");
+        scanf("%f", &c);
+     printf("Podaj promien kuli: ");
+        scanf("%f", &r);
+
+    printf("Objetosc prostopadloscianu wynosi: %.2f\n", volume_cuboid(a, b, c));
+    printf("Objetosc kuli wynosi: %.2f\n", volumeKula(r));
+    return 0;
 }
